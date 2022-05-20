6fc58c550ca8991722eb645ae73a6f1cbd56b556
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-05-21 01:18:44 +0300

    fix: исправить ошибку загрузки погоды

--- src/utils/fetchCoordinates.js
+++ src/utils/fetchCoordinates.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function fetchCoordinates(q) {
     return axios.get('/geo/1.0/direct', {
-        baseURL: 'https://api.openweathermap.org',
+        baseURL: 'http://api.openweathermap.org',
         params: {
             appid: "0e1fb26ff399f8f120e1a62839be4f02",
             limit: '1',
--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function fetchWeather({lat, lon}) {
     return axios.get('data/2.5/onecall', {
-        baseURL: 'https://api.openweathermap.org',
+        baseURL: 'http://api.openweathermap.org',
         params: {
             units: "metric",
             appid: "0e1fb26ff399f8f120e1a62839be4f02",
